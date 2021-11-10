f515dcb3ba5a26db87fcc496171ec95616b1295b
carlos <cf8838@gmail.com>
2021-11-10 17:35:39 -0500

    setup part 2

--- script.py
+++ script.py
@@ -1,16 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-
+@app.route('/')
 def home():
-    pass
+    return render_template ("home.html")
 
-@app.route("/about")
-
+@app.route('/about')
 def about():
-    pass
+    return render_template ("about.html")
 
 if __name__=="__main__":
     app.run(debug=True)
